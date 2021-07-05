66dd5e8a64dc5fd155cc9f11261043b1f8a20d65
Guido Varela Coletta <guido.varela@gmail.com>
2021-07-05 16:09:07 -0300

    clase 36

--- 29_php5-MySQL/conexion.php
+++ 29_php5-MySQL/conexion.php
@@ -1,9 +1,17 @@
 <?php
 
+//Conexion Local (Wamp)
 $host='localhost';
 $userMySQL ='root' ;
 $passwordMySQL= '';
 $bbdd='webmaster2021';
+
+//Conexion Remota (Hosting)
+/*$host='localhost';
+$userMySQL ='id11388821_bdadmin' ;
+$passwordMySQL= 'asdlaksdas5d4a46546';
+$bbdd='id11388821_usuarios';*/
+
 
 @$conexion = mysqli_connect($host,$userMySQL,$passwordMySQL,$bbdd) or exit("Error en la conexion."); //tambien podemos usar die()
 
