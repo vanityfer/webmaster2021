d963c64ad37f1e1e42c77f8ddd6282dcf0273271
Guido Varela Coletta <guido.varela@gmail.com>
2021-07-19 18:28:02 -0300

    clase 42

--- 42_nodejs/app/routes/users.js
+++ 42_nodejs/app/routes/users.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
+let users = [
+  {
+    nombre: ["Mario","Baracus"],
+    anioNac: 1952,
+    localidad: "Ituzaingo, Illinois",
+    profesion:"Militar retirado",
+    genero:1,
+    activo: true
+  },{
+    nombre: ['Carlitos',"Bala"],
+    anioNac: 1930,
+    localidad:"Caballito, CABA",
+    profesion:"Procer de la TV y de la infancia",
+    genero:1,
+    activo: false
+  },{
+    nombre:["China", "Zorrilla"],
+    anioNac: 1925,
+    localidad: "Montevideo, Uruguay",
+    profesion:'actriz',
+    genero: 0,
+    activo: false
+  },{
+    nombre:["Maria", "Moreno"],
+    anioNac: 1987,
+    localidad: "Rocha, Uruguay",
+    profesion:'panadera',
+    genero: 0,
+    activo: true
+  }
+]
+
+let title = "Usuarios"
+let date = new Date() 
+let animales = ["perro","gato","elefante"]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  res.render('usuarios.hbs',{users,title,animales, year:date.getFullYear()})
+
 });
 
 module.exports = router;
